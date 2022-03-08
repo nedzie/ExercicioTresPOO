d15f8f1427fab03d68728acbfc89d858aeb91cc0
nedzie <marcos-elivelton@hotmail.com>
2022-03-08 15:15:32 -0300

    Add project files.

---
+++ ExercicioTres.ConsoleApp/Cilindro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioTres.ConsoleApp
+{
+    internal class Cilindro
+    {
+        public double altura;
+        public double raio;
+        public double volume;
+
+        const double pi = 3.14;
+
+        public double CalcularVolume(double altura, double raio)
+        {
+            volume = pi * (raio * raio)  * altura;
+            return volume;
+        }
+    }
+}
---
+++ ExercicioTres.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExercicioTres.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cilindro azeite = new Cilindro();
+            Console.Write("Informe a altura do cilindro: \n> ");
+            azeite.altura = double.Parse(Console.ReadLine());
+            Console.Write("Informe o raio do cilindro: \n> ");
+            azeite.raio = double.Parse(Console.ReadLine());
+            azeite.volume = azeite.CalcularVolume(azeite.altura, azeite.raio);
+
+            Console.WriteLine("O volume do cilindro é de: " + azeite.volume.ToString("#.##") + "m³");
+        }
+    }
+}
